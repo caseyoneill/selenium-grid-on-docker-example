58a32f7c4c178616e25f14d70400d03b5475fae1
Casey O'Neill <j.casey.oneill@gmail.com>
2015-10-22 18:04:09 -0600

    Adding cron script to restart the grid

---
+++ cron_restart_selenium.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+LOG_DIR=/var/log/selenium
+LOG_FILE=cron_restart.log
+TIMESTAMP_FILE=last_restart.log
+MINS_SINCE_UPDATE=30
+
+#first check if selenium log directory exists
+#first check if cron log exists
+if [ ! -d $LOG_DIR ]
+then
+  #selenium log folder not found
+  echo "Selenium folder not found, creating..."
+  sudo mkdir $LOG_DIR
+  sudo chown ubuntu $LOG_DIR
+  echo ""
+fi
+
+#check if cron log exists
+if [ -f $LOG_DIR/$TIMESTAMP_FILE ]
+then
+    #log file found, check last restart timestamp
+    LAST_RESTART=$((10#$(<$LOG_DIR/$TIMESTAMP_FILE) + 1))
+    CURRENT_TIMESTAMP=$((10#$(date +%s) + 1))
+    DIFF="$(( ($CURRENT_TIMESTAMP - $LAST_RESTART)/60 ))"
+
+    if [ "$DIFF" -le "$MINS_SINCE_UPDATE" ]
+    then
+      #it hasn't been long enough between restarts, exit
+      exit 0
+    fi
+
+    echo "Restart was last run $DIFF minutes ago"
+    echo ""
+fi
+#restart grid
+date
+echo "Restarting grid"
+cd /home/ubuntu/grid
+docker-compose kill
+docker-compose up -d
+
+#log last time restarted
+#sudo bash -c 'date +%s > '"$LOG_DIR/$TIMESTAMP_FILE"
+date +%s > $LOG_DIR/$TIMESTAMP_FILE
